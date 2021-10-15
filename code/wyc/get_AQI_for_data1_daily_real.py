371fafc432184c8b4a0042a617a293cd8cd52150
wuyuchong <vector_wyc@icloud.com>
2021-10-15 10:16:45 +0800

    finish question 1

--- code/wyc/get_AQI_for_data1_daily_real.py
+++ code/wyc/get_AQI_for_data1_daily_real.py
@@ -9,7 +9,8 @@ from src.calculate_AQI import calculate_AQI
 data1_daily_real = pd.read_excel("/home/competition/math_modeling/questions/B/1.xlsx", sheet_name="监测点A逐日污染物浓度实测数据")
 dict_range = pd.read_csv('./input/range.csv').to_dict(orient = 'list')
 
-data = data1_daily_real.copy()
-calculate_AQI(data, dict_range)
-print(data)
+data = calculate_AQI(data1_daily_real, dict_range)
+
+data.to_csv('./output/data1_daily_real_AQI.csv', index=False)
+data.to_excel('./output/data1_daily_real_AQI.xlsx', index=False)
 
